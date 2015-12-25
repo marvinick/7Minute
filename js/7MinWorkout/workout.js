2dca660e6fa110a88f6800d82777b4cd1ae6a6dd
marvin <marvkipi@gmail.com>
2015-12-25 13:04:44 -0600

    interval promise

--- js/7MinWorkout/workout.js
+++ js/7MinWorkout/workout.js
@@ -22,15 +22,16 @@ angular.module('7minWorkout')
       function Exercise(args) {
           this.name = args.name;
           this.title = args.title;
-          this.description = args.description; 
+          this.description = args.description;
           this.image = args.image;
-          this.related = {};
+          this.related = {}; 
           this.related.videos = args.videos;
           this.nameSound = args.nameSound;
           this.procedure = args.procedure;
       }
 
       var restExercise;
+      var exerciseIntervalPromise;
       var startWorkout = function () {
           $scope.workoutPlan = createWorkout();
           $scope.workoutTimeRemaining = $scope.workoutPlan.totalWorkoutDuration();
@@ -43,9 +44,6 @@ angular.module('7minWorkout')
               }),
               duration: $scope.workoutPlan.restBetweenExercise
           };
-          $interval(function () {
-              $scope.workoutTimeRemaining = $scope.workoutTimeRemaining - 1;
-          }, 1000, $scope.workoutTimeRemaining);
 
           $scope.currentExerciseIndex = -1;
           startExercise($scope.workoutPlan.exercises[0]);
@@ -59,19 +57,9 @@ angular.module('7minWorkout')
               $scope.currentExerciseIndex++;
           }
 
-          $interval(function () {
-              ++$scope.currentExerciseDuration;
-          }, 1000, $scope.currentExercise.duration)
-          .then(function () {
-              var next = getNextExercise(exercisePlan);
-              if (next) {
-                  startExercise(next);
-              }
-              else {
-                  $location.path('/finish');
-              }
-          });
+          exerciseIntervalPromise = startExerciseTimeTracking();
       };
+
       var getNextExercise = function (currentExercisePlan) {
           var nextExercise = null;
           if (currentExercisePlan === restExercise) {
@@ -85,7 +73,50 @@ angular.module('7minWorkout')
           return nextExercise;
       };
 
-      var exerciseIntervalPromise;
+      $scope.pauseWorkout = function () {
+          $interval.cancel(exerciseIntervalPromise);
+          $scope.workoutPaused = true;
+      };
+
+      $scope.resumeWorkout = function () {
+          exerciseIntervalPromise = startExerciseTimeTracking();
+          $scope.workoutPaused = false;
+      };
+
+      $scope.pauseResumeToggle = function () {
+          if ($scope.workoutPaused) {
+              $scope.resumeWorkout();
+          }
+          else {
+              $scope.pauseWorkout();
+          }
+      }
+
+      var startExerciseTimeTracking = function () {
+          var promise = $interval(function () {
+              ++$scope.currentExerciseDuration;
+              --$scope.workoutTimeRemaining;
+          }, 1000, $scope.currentExercise.duration - $scope.currentExerciseDuration);
+
+          promise.then(function () {
+              var next = getNextExercise($scope.currentExercise);
+              if (next) {
+                  startExercise(next);
+              }
+              else {
+                  $location.path('/finish');
+              }
+          }, function (error) {
+              console.log('Inteval promise cancelled. Error reason -' + error);
+          });
+          return promise;
+      }
+
+      $scope.onKeyPressed = function (event) {
+          if (event.which == 80 || event.which == 112) {        // 'p' or 'P' key to toggle pause and resume.
+              $scope.pauseResumeToggle();
+          }
+      };
 
       //$scope.$watch('currentExerciseDuration', function (nVal) {
       //    if (nVal == $scope.currentExercise.duration) {
@@ -111,12 +142,13 @@ angular.module('7minWorkout')
                   title: "Jumping Jacks",
                   description: "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
                   image: "img/JumpingJacks.png",
+                  nameSound: "content/jumpingjacks.wav",
                   videos: ["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"],
                   procedure: "Assume an erect position, with feet together and arms at your side.\
                             <br/>Slightly bend your knees, and propel yourself a few inches into the air.\
                             <br/>While in air, bring your legs out to the side about shoulder width or slightly wider.\
-                            As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.\
-                            Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent"
+                            <br/>As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.\
+                            <br/>Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent"
               }),
               duration: 30
           });
@@ -126,11 +158,12 @@ angular.module('7minWorkout')
                   title: "Wall Sit",
                   description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
                   image: "img/wallsit.png",
+                  nameSound: "content/wallsit.wav",
                   videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
                   procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
-                              Then, keeping your back against the wall, lower your hips until your knees form right angles. "
+                              <br/>Then, keeping your back against the wall, lower your hips until your knees form right angles. "
               }),
-              duration: 30 
+              duration: 30
           });
           workout.exercises.push({
               details: new Exercise({
@@ -138,6 +171,7 @@ angular.module('7minWorkout')
                   title: "Push Up",
                   description: "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
                   image: "img/Pushup.png",
+                  nameSound: "content/pushups.wav",
                   videos: ["//www.youtube.com/embed/Eh00_rniF8E", "//www.youtube.com/embed/ZWdBqFLNljc", "//www.youtube.com/embed/UwRLWMcOdwI", "//www.youtube.com/embed/ynPwl6qyUNM", "//www.youtube.com/embed/OicNTT2xzMI"],
                   procedure: "Lie prone on the ground with hands placed as wide or slightly wider than shoulder width. \
                               Keeping the body straight, lower body to the ground by bending arms at the elbows. \
@@ -151,6 +185,7 @@ angular.module('7minWorkout')
                   title: "Abdominal Crunches",
                   description: "The basic crunch is a abdominal exercise in a strength-training program.",
                   image: "img/crunches.png",
+                  nameSound: "content/crunches.wav",
                   videos: ["//www.youtube.com/embed/Xyd_fa5zoEU", "//www.youtube.com/embed/MKmrqcoCZ-M"],
                   procedure: "Lie on your back with your knees bent and feet flat on the floor, hip-width apart.\
                               Place your hands behind your head so your thumbs are behind your ears.\
@@ -167,6 +202,7 @@ angular.module('7minWorkout')
                   title: "Step Up Onto Chair",
                   description: "Step exercises are ideal for building muscle in your lower body.",
                   image: "img/stepUpOntoChair.png",
+                  nameSound: "content/stepup.wav",
                   videos: ["//www.youtube.com/embed/aajhW7DD1EA"],
                   procedure: "Position your chair in front of you.\
                               Stand with your feet about hip width apart, arms at your sides.\
@@ -181,6 +217,7 @@ angular.module('7minWorkout')
                   title: "Squat",
                   description: "The squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips, buttocks and quads.",
                   image: "img/squat.png",
+                  nameSound: "content/squats.wav",
                   videos: ["//www.youtube.com/embed/QKKZ9AGYTi4", "//www.youtube.com/embed/UXJrBgI2RxA"],
                   procedure: "Stand with your head facing forward and your chest held up and out.\
                               Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.\
@@ -196,6 +233,7 @@ angular.module('7minWorkout')
                   title: "Tricep Dips On Chair",
                   description: "A body weight exercise that targets triceps.",
                   image: "img/tricepdips.png",
+                  nameSound: "content/tricepdips.wav",
                   videos: ["//www.youtube.com/embed/tKjcgfu44sI", "//www.youtube.com/embed/jox1rb5krQI"],
                   procedure: "Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.\
                               Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.\
@@ -210,6 +248,7 @@ angular.module('7minWorkout')
                   title: "Plank",
                   description: "The plank (also called a front hold, hover, or abdominal bridge) is an isometric core strength exercise that involves maintaining a difficult position for extended periods of time. ",
                   image: "img/Plank.png",
+                  nameSound: "content/plank.wav",
                   videos: ["//www.youtube.com/embed/pSHjTRCQxIw", "//www.youtube.com/embed/TvxNkmjdhMM"],
                   procedure: "Get into pushup position on the floor.\
                               Bend your elbows 90 degrees and rest your weight on your forearms.\
@@ -224,6 +263,7 @@ angular.module('7minWorkout')
                   title: "High Knees",
                   description: "A form exercise that develops strength and endurance of the hip flexors and quads and stretches the hip extensors.",
                   image: "img/highknees.png",
+                  nameSound: "content/highknees.wav",
                   videos: ["//www.youtube.com/embed/OAJ_J3EZkdY", "//www.youtube.com/embed/8opcQdC-V-U"],
                   procedure: "Start standing with feet hip-width apart. \
                               Do inplace jog with your knees lifting as much as possible towards your chest."
@@ -236,6 +276,7 @@ angular.module('7minWorkout')
                   title: "Lunges",
                   description: "Lunges are a good exercise for strengthening, sculpting and building several muscles/muscle groups, including the quadriceps (or thighs), the gluteus maximus (or buttocks) as well as the hamstrings. ",
                   image: "img/lunges.png",
+                  nameSound: "content/lunge.wav",
                   videos: ["//www.youtube.com/embed/Z2n58m2i4jg"],
                   procedure: "Stand erect with your feet about one shoulder width apart.\
                               Put your hands on your hips, keep your back as straight as possible, relax your shoulders and keep your eyes facing directly ahead.\
@@ -252,6 +293,7 @@ angular.module('7minWorkout')
                   title: "Pushup And Rotate",
                   description: "A variation of pushup that requires you to rotate.",
                   image: "img/pushupNRotate.png",
+                  nameSound: "content/pushupandrotate.wav",
                   videos: ["//www.youtube.com/embed/qHQ_E-f5278"],
                   procedure: "Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.\
                               Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling."
@@ -264,6 +306,7 @@ angular.module('7minWorkout')
                   title: "Side Plank",
                   description: "A variation to Plank done using one hand only",
                   image: "img/sideplank.png",
+                  nameSound: "content/sideplank.wav",
                   videos: ["//www.youtube.com/embed/wqzrb67Dwf8", "//www.youtube.com/embed/_rdfjFSFKMY"],
                   procedure: "Lie on your side, in a straight line from head to feet, resting on your forearm.\
                               Your elbow should be directly under your shoulder.\
@@ -283,44 +326,60 @@ angular.module('7minWorkout')
   }]);
 
 angular.module('7minWorkout')
-   .controller('WorkoutAudioController', ['$scope', '$timeout',
-     function ($scope, $timeout) {
-    $scope.exercisesAudio = [];
+  .controller('WorkoutAudioController', ['$scope', '$interval', '$location', '$timeout', function ($scope, $interval, $location, $timeout) {
+      $scope.exercisesAudio = [];
 
-    var workoutPlanwatch = $scope.$watch('workoutPlan', function (newValue, oldValue) {
-        if (newValue) {
-            angular.forEach($scope.workoutPlan.exercises, function (exercise) {
-                $scope.exercisesAudio.push({ src: exercise.details.nameSound, type: "audio/wav" });
-            });
-            workoutPlanwatch(); //unbind the watch 
-        }
-    });
+      var workoutPlanwatch = $scope.$watch('workoutPlan', function (newValue, oldValue) {
+          if (newValue) {
+              angular.forEach($scope.workoutPlan.exercises, function (exercise) {
+                  $scope.exercisesAudio.push({ src: exercise.details.nameSound, type: "audio/wav" });
+              });
+              workoutPlanwatch();       //unbind the watch.
+          }
+      });
 
-    $scope.$watch('currentExercise', function (newValue, oldValue) {
-        if (newValue && newValue != oldValue) {
-            if ($scope.currentExercise.details.name == 'rest') {
-                $timeout(function () {
-                    $scope.nextUpAudio.play();
-                }, 2000);
-                $timeout(function () {
-                    $scope.nextUpExerciseAudio.play($scope.currentExerciseIndex + 1, true);
-                }, 3000);
-            }
-        }
-    }); 
+      $scope.$watch('currentExercise', function (newValue, oldValue) {
+          if (newValue && newValue != oldValue) {
+              if ($scope.currentExercise.details.name == 'rest') {
+                  $timeout(function () {
+                      $scope.nextUpAudio.play();
+                  }, 2000);
+                  $timeout(function () {
+                      $scope.nextUpExerciseAudio.play($scope.currentExerciseIndex + 1, true);
+                  }, 3000);
+              }
+          }
+      });
 
-    $scope.$watch('currentExerciseDuration', function (newValue, oldValue) {
-        if (newValue) {
-            if (newValue == $scope.currentExercise.duration / 2 && $scope.currentExercise.details.name != 'rest') {
-                $scope.halfWayAudio.play();
-            } 
-            else if (newValue == $scope.currentExercise.duration - 3) {
-                $scope.aboutToCompleteAudio.play();
-            }
-        }
-    });
+      $scope.$watch('currentExerciseDuration', function (newValue, oldValue) {
+          if (newValue) {
+              if (newValue == Math.floor($scope.currentExercise.duration / 2) && $scope.currentExercise.details.name != 'rest') {
+                  $scope.halfWayAudio.play();
+              }
+              else if (newValue == $scope.currentExercise.duration - 3) {
+                  $scope.aboutToCompleteAudio.play();
+              }
+          }
+      });
 
-    var init = function () {
-    }
-  init(); 
-}]);
+      $scope.$watch('workoutPaused', function (newValue, oldValue) {
+          if (newValue) {
+              $scope.ticksAudio.pause();
+              $scope.nextUpAudio.pause();
+              $scope.nextUpExerciseAudio.pause();
+              $scope.halfWayAudio.pause();
+              $scope.aboutToCompleteAudio.pause();
+          }
+          else {
+              $scope.ticksAudio.play();
+              if ($scope.halfWayAudio.currentTime > 0 && $scope.halfWayAudio.currentTime < $scope.halfWayAudio.duration) $scope.halfWayAudio.play();
+              if ($scope.aboutToCompleteAudio.currentTime > 0 && $scope.aboutToCompleteAudio.currentTime < $scope.aboutToCompleteAudio.duration) $scope.aboutToCompleteAudio.play();
+          }
+      });
+
+      var init = function () {
+      }
+
+      init();
+
+  }]);
